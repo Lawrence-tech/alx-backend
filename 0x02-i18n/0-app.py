a5783bb35541ee06a31d851417b3288cba7e96a5
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-29 18:27:16 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,11 +8,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/", strict_slashes=False)
 def index():
     """Render the index.html template."""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
