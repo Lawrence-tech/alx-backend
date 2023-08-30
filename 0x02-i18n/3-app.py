96fbba13d794bcf56a2b59240e7bbe1b2080c6c9
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-30 14:48:01 +0000

    document gettext function

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -8,7 +8,7 @@ from flask_babel import Babel, gettext
 
 class Config(object):
     """
-    Config class
+    Config class  that has a LANGUAGES class attribute equal to en/fr
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -24,7 +24,7 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """
-    Sets the best match language
+    Sets the best match language with the babel.localeselector decorator.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
